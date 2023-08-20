7b83220b8e43518633604295a1fe25d41acb17eb
Marius <mariolee303@gmail.com>
2023-08-20 16:40:34 +0300

    small z fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         <ThemeContextProvider>
-          <div className=' inset-0 absolute overflow-hidden w-full h-[100%]'>
+          <div className='z-[-2] inset-0 absolute overflow-hidden w-full h-[100%]'>
                     <BackgroundBlob color='#C9C3F9' colorDark='#676394' position='left'/>
         <BackgroundBlob color='#F9D2D3' colorDark='#946263' position='right'/>
           </div>
